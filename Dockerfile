4a7b290ea054aea0f382bca4d7b4cd79bc2d2f01
IONTRADING\rohit.sinha <rohit.sinha@lab49.com>
2024-07-09 14:12:47 +0530

    COde modified to run scripts in docker container

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
 FROM openjdk:11-jre-slim
+RUN apt-get update && apt-get install -y curl jq
 WORKDIR C:/Projects/SeleniumManagerProject
-COPY C:/Projects/SeleniumManagerProject C:/Projects/SeleniumManagerProject
+COPY target/selenium-docker.jar selenium-docker.jar
+COPY target/selenium-docker-tests.jar selenium-docker-tests.jar
+COPY target/libs libs
 COPY testng.xml testng.xml
-RUN apk add --no-cache maven
-RUN mvn dependency:get -Dartifact=org.testng:testng:7.5
-ENTRYPOINT ["java", "-cp", "/root/.m2/repository/org/testng/testng/7.7.1/testng-7.7.1.jar:/app", "org.testng.TestNG", "testng.xml"]
+#ENTRYPOINT java -cp selenium-docker.jar;selenium-docker-tests.jar;libs/* -DHUB_HOST=$HUB_HOST org.testng.TestNG $MODULE
+ADD healthcheck.sh healthcheck.sh
+ENTRYPOINT sh healthcheck.sh
--- src/test/java/com/example/tests/BaseTest.java
+++ src/test/java/com/example/tests/BaseTest.java
@@ -1,6 +1,7 @@
 package com.example.tests;
 
 import com.example.utility.Util;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Attachment;
 import org.apache.commons.io.FileUtils;
@@ -8,8 +9,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
@@ -17,6 +21,8 @@ import org.testng.annotations.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.logging.Logger;
 
@@ -27,13 +33,33 @@ public class BaseTest {
     @BeforeClass
     //@BeforeMethod
     @Parameters({"browser"})
-    public void setUp (String browser) {
+    public void setUp (String browser) throws MalformedURLException {
         System.out.println ("Browser is : "+browser);
+        String host = "localhost";
+        if(System.getProperty("HUB_HOST")!=null) {
+            host=System.getProperty("HUB_HOST");
+
+        }
+        String completeURL="http://" + host +":4444/wd/hub";
+        System.out.println("Complete URL : "+completeURL);
         if(browser.equalsIgnoreCase ("chrome")) {
-            driver.set (new ChromeDriver ());
+            System.out.println ("Inside : "+browser);
+            ChromeOptions options = new ChromeOptions ();
+            options.setAcceptInsecureCerts (true);
+
+            DesiredCapabilities desiredCapabilities = new DesiredCapabilities ();
+            desiredCapabilities.setCapability (ChromeOptions.CAPABILITY,options);
+
+            URL url = new URL ("http://localhost:4444/wd/hub");
+
+            driver.set (new RemoteWebDriver (new URL (completeURL),desiredCapabilities));
+            System.out.println("Driver value is : "+driver.get ());
+            System.out.println("URL is : "+driver.get ().getCurrentUrl ());
         }else if (browser.equalsIgnoreCase ("firefox")){
+            WebDriverManager.firefoxdriver ().setup();
             driver.set (new FirefoxDriver ());
         }else if (browser.equalsIgnoreCase ("edge")){
+            WebDriverManager.edgedriver ().setup();
             driver.set (new EdgeDriver ());
         }else {
             throw new IllegalArgumentException ("Browser value is not supported : "+browser);
@@ -44,8 +70,9 @@ public class BaseTest {
     @AfterClass
     //@AfterMethod
     public void tearDown () {
-        if (driver.get () != null) {
-            driver.get ().quit ();
+        WebDriver webDriver = getDriver ();
+        if (webDriver != null) {
+            webDriver.quit();
             driver.remove ();
         }
     }
@@ -75,7 +102,7 @@ public class BaseTest {
         new WebDriverWait(getDriver (), Duration.ofSeconds (10000)).until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 
-    @AfterMethod (alwaysRun = true)
+    @AfterMethod (alwaysRun = true,enabled = true)
     public synchronized  void updateTestStatus(ITestResult result) throws IOException {
         Logger.getGlobal ().info ("Updating result of test script "+result.getName ()+" to report :: updateTestStatus");
         System.out.println("Driver value is : "+driver.get ());
