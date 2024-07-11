fa7c29d88ab92bed93701b305c481ae0c883c6f6
IONTRADING\rohit.sinha <rohit.sinha@lab49.com>
2024-07-11 18:53:09 +0530

    UtilFiles folder created

--- src/test/java/com/example/utility/GitOperations.java
+++ src/test/java/com/example/utility/GitOperations.java
@@ -1,11 +1,12 @@
 package com.example.utility;
 
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.api.errors.InvalidRemoteException;
-import org.eclipse.jgit.api.errors.TransportException;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.eclipse.jgit.transport.SshTransport;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.ssh.jsch.JschConfigSessionFactory;
+import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig;
+import com.jcraft.jsch.Session;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,12 +15,19 @@ public class GitOperations {
 
     public static void main(String[] args) {
         String localRepoPath = "C:\\Projects\\SeleniumManagerProject";
-        String remoteRepoUri = "https://github.com/rohit-sinha-lab49/WebSeleniumAutomationFramework.git";
+        String remoteRepoUri = "git@github.com:rohit-sinha-lab49/WebSeleniumAutomationFramework.git";
         String branchName = "featureOne";
-        String username = "rohit-sinha-lab49";
-        String password = "Sensex$#12345";
+        String commitMessage = "UtilFiles folder created";
 
         try {
+            // Setup SSH session factory
+            SshSessionFactory.setInstance(new JschConfigSessionFactory () {
+                @Override
+                protected void configure(OpenSshConfig.Host host, Session session) {
+                    // Configure SSH session if necessary
+                }
+            });
+
             // Open local repository
             File localRepo = new File(localRepoPath);
             Git git = Git.open(localRepo);
@@ -31,26 +39,28 @@ public class GitOperations {
             git.add().addFilepattern(".").call();
 
             // Commit changes
-            git.commit().setMessage("Your commit message").call();
+            git.commit().setMessage(commitMessage).call();
 
-            // Pull latest changes from current_branch
-            PullResult pullResult = git.pull()
+            // Pull latest changes from master
+            git.pull()
+                    .setTransportConfigCallback(transport -> {
+                        if (transport instanceof SshTransport) {
+                            SshTransport sshTransport = (SshTransport) transport;
+                            sshTransport.setSshSessionFactory(SshSessionFactory.getInstance());
+                        }
+                    })
                     .setRemote("origin")
                     .setRemoteBranchName("featureOne")
-                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password))
                     .call();
-
-            if (pullResult.isSuccessful()) {
-                System.out.println("Pull successful.");
-            } else {
-                System.out.println("Pull failed: " + pullResult.toString());
-                return;
-            }
 
             // Push local changes to master
             git.push()
-                    .setRemote("origin")
-                    .setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password))
+                    .setTransportConfigCallback(transport -> {
+                        if (transport instanceof SshTransport) {
+                            SshTransport sshTransport = (SshTransport) transport;
+                            sshTransport.setSshSessionFactory(SshSessionFactory.getInstance());
+                        }
+                    })
                     .call();
 
             System.out.println("Push successful.");
