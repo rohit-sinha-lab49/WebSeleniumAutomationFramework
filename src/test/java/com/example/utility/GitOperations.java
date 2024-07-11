01f77ddff7813e8c42c9d0bf2ef85e2d36fd03a4
IONTRADING\rohit.sinha <rohit.sinha@lab49.com>
2024-07-11 19:02:00 +0530

    UtilFiles folder created

--- src/test/java/com/example/utility/GitOperations.java
+++ src/test/java/com/example/utility/GitOperations.java
@@ -1,5 +1,7 @@
 package com.example.utility;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.transport.SshTransport;
@@ -7,6 +9,7 @@ import org.eclipse.jgit.transport.SshSessionFactory;
 import org.eclipse.jgit.transport.ssh.jsch.JschConfigSessionFactory;
 import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig;
 import com.jcraft.jsch.Session;
+import org.eclipse.jgit.util.FS;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +17,32 @@ import java.io.IOException;
 public class GitOperations {
 
     public static void main(String[] args) {
+
+        SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+            @Override
+            protected void configure(OpenSshConfig.Host hc, Session session) {
+                session.setConfig("StrictHostKeyChecking", "no");
+            }
+
+            @Override
+            protected JSch getJSch(OpenSshConfig.Host hc, FS fs) throws JSchException {
+                JSch jsch = super.getJSch(hc, fs);
+                jsch.addIdentity(System.getProperty("user.home") + "/.ssh/id_rsa");
+                return jsch;
+            }
+        };
+
+        try (Git git = Git.open(new File("C:\\Projects\\SeleniumManagerProject"))) {
+            git.push()
+                    .setTransportConfigCallback(transport -> {
+                        SshTransport sshTransport = (SshTransport) transport;
+                        sshTransport.setSshSessionFactory(sshSessionFactory);
+                    })
+                    .call();
+        } catch (IOException | GitAPIException e) {
+            e.printStackTrace();
+        }
+
         String localRepoPath = "C:\\Projects\\SeleniumManagerProject";
         String remoteRepoUri = "git@github.com:rohit-sinha-lab49/WebSeleniumAutomationFramework.git";
         String branchName = "featureOne";
@@ -21,12 +50,15 @@ public class GitOperations {
 
         try {
             // Setup SSH session factory
-            SshSessionFactory.setInstance(new JschConfigSessionFactory () {
+            /*SshSessionFactory.setInstance(new JschConfigSessionFactory () {
                 @Override
                 protected void configure(OpenSshConfig.Host host, Session session) {
                     // Configure SSH session if necessary
+                    session.setConfig("StrictHostKeyChecking", "no");
                 }
-            });
+
+
+            });*/
 
             // Open local repository
             File localRepo = new File(localRepoPath);
