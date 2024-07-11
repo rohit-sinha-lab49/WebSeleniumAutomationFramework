8e153947eb2bb276d8c1502841610c08c9dbab80
IONTRADING\rohit.sinha <rohit.sinha@lab49.com>
2024-07-11 20:40:38 +0530

    Your commit message

--- src/test/java/com/example/utility/GitOperations.java
+++ src/test/java/com/example/utility/GitOperations.java
@@ -1,105 +1,75 @@
 package com.example.utility;
 
-import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.transport.SshTransport;
+import org.eclipse.jgit.api.errors.JGitInternalException;
 import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 import org.eclipse.jgit.transport.ssh.jsch.JschConfigSessionFactory;
 import org.eclipse.jgit.transport.ssh.jsch.OpenSshConfig;
-import com.jcraft.jsch.Session;
 import org.eclipse.jgit.util.FS;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GitOperations {
-
     public static void main(String[] args) {
-
-        SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+        // Set up the custom SSH session factory
+        SshSessionFactory.setInstance(new JschConfigSessionFactory () {
             @Override
-            protected void configure(OpenSshConfig.Host hc, Session session) {
-                session.setConfig("StrictHostKeyChecking", "no");
+            protected void configure(OpenSshConfig.Host host, Session session) {
+                // Use the default configuration
             }
 
             @Override
-            protected JSch getJSch(OpenSshConfig.Host hc, FS fs) throws JSchException {
-                JSch jsch = super.getJSch(hc, fs);
+            protected JSch createDefaultJSch(FS fs) throws JSchException {
+                JSch jsch = super.createDefaultJSch(fs);
                 jsch.addIdentity(System.getProperty("user.home") + "/.ssh/id_rsa");
                 return jsch;
             }
-        };
-
-        try (Git git = Git.open(new File("C:\\Projects\\SeleniumManagerProject"))) {
-            git.push()
-                    .setTransportConfigCallback(transport -> {
-                        SshTransport sshTransport = (SshTransport) transport;
-                        sshTransport.setSshSessionFactory(sshSessionFactory);
-                    })
-                    .call();
-        } catch (IOException | GitAPIException e) {
-            e.printStackTrace();
-        }
-
-        String localRepoPath = "C:\\Projects\\SeleniumManagerProject";
-        String remoteRepoUri = "git@github.com:rohit-sinha-lab49/WebSeleniumAutomationFramework.git";
-        String branchName = "featureOne";
-        String commitMessage = "UtilFiles folder created";
+        });
 
         try {
-            // Setup SSH session factory
-            /*SshSessionFactory.setInstance(new JschConfigSessionFactory () {
-                @Override
-                protected void configure(OpenSshConfig.Host host, Session session) {
-                    // Configure SSH session if necessary
-                    session.setConfig("StrictHostKeyChecking", "no");
-                }
+            // Clone the repository (if not already cloned)
+            // Git.cloneRepository()
+            // .setURI("git@github.com:username/repository.git")
+            // .setDirectory(new File("/path/to/repo"))
+            // .call();
 
+            File repoDir = new File("C:/Projects/SeleniumManagerProject");
 
-            });*/
+            File lockFile = new File(repoDir, ".git/index.lock");
+            if (lockFile.exists()) {
+                Files.delete(Paths.get(lockFile.getAbsolutePath()));
+                System.out.println("Deleted existing index.lock file.");
+            }
 
-            // Open local repository
-            File localRepo = new File(localRepoPath);
-            Git git = Git.open(localRepo);
+            // Open the existing repository
+            Git git = Git.open(repoDir);
 
-            // Checkout to the branch
-            git.checkout().setName(branchName).call();
-
-            // Add changes to staging area
+            // Add all files to the staging area
             git.add().addFilepattern(".").call();
 
-            // Commit changes
-            git.commit().setMessage(commitMessage).call();
+            // Commit the changes
+            git.commit().setMessage("Your commit message").call();
 
-            // Pull latest changes from master
-            git.pull()
-                    .setTransportConfigCallback(transport -> {
-                        if (transport instanceof SshTransport) {
-                            SshTransport sshTransport = (SshTransport) transport;
-                            sshTransport.setSshSessionFactory(SshSessionFactory.getInstance());
-                        }
-                    })
-                    .setRemote("origin")
-                    .setRemoteBranchName("featureOne")
-                    .call();
+            // Push the changes to the remote repository
+            PushCommand pushCommand = git.push();
+            pushCommand.setCredentialsProvider(new UsernamePasswordCredentialsProvider("rohit-sinha-lab49", "Sensex$#12345"));
+            pushCommand.call();
 
-            // Push local changes to master
-            git.push()
-                    .setTransportConfigCallback(transport -> {
-                        if (transport instanceof SshTransport) {
-                            SshTransport sshTransport = (SshTransport) transport;
-                            sshTransport.setSshSessionFactory(SshSessionFactory.getInstance());
-                        }
-                    })
-                    .call();
-
-            System.out.println("Push successful.");
-
-        } catch (IOException | GitAPIException e) {
+        } catch (IOException e) {
+            System.err.println("IOException: " + e.getMessage());
+            e.printStackTrace();
+        } catch (GitAPIException e) {
+            System.err.println("GitAPIException: " + e.getMessage());
             e.printStackTrace();
         }
     }
 }
-
