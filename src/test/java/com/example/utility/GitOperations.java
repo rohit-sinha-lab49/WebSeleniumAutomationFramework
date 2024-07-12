329dd93ce8e7703659a134f1001fe638bf1ace28
IONTRADING\rohit.sinha <rohit.sinha@lab49.com>
2024-07-12 11:08:55 +0530

    Your commit message

--- src/test/java/com/example/utility/GitOperations.java
+++ src/test/java/com/example/utility/GitOperations.java
@@ -27,6 +27,7 @@ public class GitOperations {
             protected void configure(OpenSshConfig.Host host, Session session) {
                 // Use the default configuration
 
+
             }
 
             @Override
